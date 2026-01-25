91e7f6b659495cd117553599c1add1330d17e210
Matvey <qazsewpp@gmail.com>
2026-01-25 17:34:13 +0000

    some cmake

---
+++ services/include/async/async_task.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <coroutine>
+
+template<typename T>
+class TAsyncTask {
+private:
+    struct promise_type {
+        T Value;
+        std::exception_ptr Exception;
+        std::courotine_handle<> Handle;
+
+        TAsyncTask get_return_object() { 
+            return {TAsyncTask<T>::from_promise(*this)}; 
+        }
+
+        std::suspend_always initial_suspend() noexcept { return {}; }
+
+        std::suspend_always final_suspend() noexcept { return {}; }
+
+        void return_value(T val) {
+            Value = std::move(val);
+        }
+
+        void unhandled_exception() {
+            Exception = std::current_exception();
+        }
+    };
+
+};
