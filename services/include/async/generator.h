ecb96ad5097081a2e40f0284f9de2e9936dea053
Matvey <qazsewpp@gmail.com>
2026-01-27 19:26:48 +0000

    upd

---
+++ services/include/async/generator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <coroutine>
+#include <exception>
+
+template<typename T>
+class TGenerator {
+private:
+    struct promise_type {
+        T Value;
+        std::exception_ptr Exception;
+        std::coroutine_handle<> Handle;
+
+        TGenerator get_return_object() { 
+            return {}; 
+        }
+
+        std::suspend_always initial_suspend() noexcept { return {}; }
+
+        std::suspend_always final_suspend() noexcept { return {}; }
+
+        void return_value(T val) {
+            Value = std::move(val);
+        }
+
+        void unhandled_exception() {
+            Exception = std::current_exception();
+        }
+    };
+
+};
