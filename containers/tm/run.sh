b86ad3508bb568d6c26dde923ec77ae245eb5963
Matvey <qazsewpp@gmail.com>
2026-07-09 15:23:23 +0000

    upd containers

--- containers/tm/run.sh
+++ containers/tm/run.sh
@@ -2,11 +2,21 @@
 
 server=$1
 
-sed "s|<password_sa></password>|<password>$(sec $server-secrets master_pw)</password>|g" GameData/Config/dedicated_cfg.txt > GameData/Config/new_cfg
-sed -i "s|<password_ad></password>|<password>$(sec $server-secrets admin_pw)</password>|g" GameData/Config/new_cfg
-sed -i "s|<password_us></password>|<password>$(sec $server-secrets user_pw)</password>|g" GameData/Config/new_cfg
-sed -i "s|<password_server></password>|<password>$(sec $server-secrets server_pw)</password>|g" GameData/Config/new_cfg
+sed "s|<password_sa></password>|<password>$(./get_secret.sh $server-secrets master_pw)</password>|g" GameData/Config/dedicated_cfg.txt > GameData/Config/new_cfg
+sed -i "s|<password_ad></password>|<password>$(./get_secret.sh $server-secrets admin_pw)</password>|g" GameData/Config/new_cfg
+sed -i "s|<login_server></login>|<login>$server</login>|g" GameData/Config/new_cfg
+sed -i "s|<name_server></name>|<name>$server</name>|g" GameData/Config/new_cfg
+sed -i "s|<password_us></password>|<password>$(./get_secret.sh $server-secrets user_pw)</password>|g" GameData/Config/new_cfg
+sed -i "s|<password_server></password>|<password>$(./get_secret.sh $server-secrets server_pw)</password>|g" GameData/Config/new_cfg
 
 cat GameData/Config/new_cfg
 
-./TrackmaniaServer /game_settings=MatchSettings/CustomSettings/cswh.txt /dedicated_cfg=new_cfg
+./TrackmaniaServer /game_settings=MatchSettings/CustomSettings/cswh.txt /dedicated_cfg=new_cfg > logs
+
+sleep 1
+
+SERVER_PID=$(grep -o 'pid=[0-9]*' logs | head -n 1 | cut -d= -f2)
+
+while kill -0 "$SERVER_PID" 2>/dev/null; do
+    sleep 5
+done
--- containers/xaseco/run.sh
+++ containers/xaseco/run.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-php aseco.php TMF </dev/null >aseco.log 2>&1 &
-echo $!
+server=$1
+
+sed -i "s|<password_sa></password>|<password>$(./get_secret.sh $server-secrets master_pw)</password>|g" config.xml
+sed -i "s|<ip>127.0.0.1</ip>|<ip>$server-base</ip>|g" config.xml
+sed -i "s|<password_server></password>|<password>$(./get_secret.sh $server-secrets server_pw)</password>|g" dedimania.xml
+
+sed -i "s|<mysql_server></mysql_server>|<mysql_server>$server-mysql</mysql_server>|g" localdatabase.xml
+sed -i "s|<mysql_password></mysql_password>|<mysql_password>$(./get_secret.sh $server-secrets mysql_pw)</mysql_password>|g" localdatabase.xml
+
+sed -i "s|<login></login>|<login>$server</login>|g" dedimania.xml
+
+php aseco.php TMF </dev/null >&1 2>&1 &
+
+SERVER_PID=$!
+
+while kill -0 "$SERVER_PID" 2>/dev/null; do
+    sleep 5
+done
